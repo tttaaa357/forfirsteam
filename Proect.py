d7cfccaacb009617b9390e0ff1efcaadff4068f9
Your Name <tttaaa357@gmail.com>
2024-04-14 11:53:37 +0300

    База данных 1

--- Proect.py
+++ Proect.py
@@ -1,7 +1,6 @@
+from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import Integer, String, Float, Boolean
 from base import Base
-from sqlalchemy.orm import Mapped, mapped_column
-
 
 class DB(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
