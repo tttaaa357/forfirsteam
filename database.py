b362400570c69e6f7fdb60d60a5cdc7258a28724
Your Name <tttaaa357@gmail.com>
2024-04-14 13:07:53 +0300

    Update database.py

--- database.py
+++ database.py
@@ -7,5 +7,6 @@ class DB(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String, nullable=False)
     surname: Mapped[str] = mapped_column(String, nullable=False)
-    napravlenie: Mapped[bool] = mapped_column(Boolean, nullable=False)
-    tgid: Mapped[int] = mapped_column(Integer, primary_key=True)
+    napravlenie: Mapped[str] = mapped_column(String, nullable=False)
+    opit: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    tgid: Mapped[int] = mapped_column(Integer)
